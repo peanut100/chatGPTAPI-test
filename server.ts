fd9a8ba385085c583a4955fb1715d6d424b9f907
peanut100 <peanut.walnut100@gmail.com>
2024-03-13 09:35:29 +0900

    js→ts

--- server.ts
+++ server.ts
@@ -7,7 +7,6 @@ app.use(express.json());
 app.use(cors());
 
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
-app.use(express.json());
 app.post("/chat", async (req, res) => {
   try {
     const response = await openai.chat.completions.create({
@@ -15,15 +14,17 @@ app.post("/chat", async (req, res) => {
       messages: [
         {
           role: "user",
-          content: "あなたは褒め上手です。これから送られるメッセージに対して、すべて肯定的な意見を言い、褒めてください！メッセージに特に褒めるべき所がない場合でも生きているだけで偉い！みたいな感じで、すべてを肯定してください" + req.body.message,
+          content: req.body.message,
         },
-      ], // フロントエンドから送られてきたメッセージを使用
+      ],
       stream: true,
     });
 
     let responseContent = "";
     for await (const chunk of response) {
+      console.log(response, "response");
       responseContent += chunk.choices[0]?.delta?.content || "";
+      console.log(responseContent, "responseContent");
     }
 
     // フロントエンドにレスポンスを送信
@@ -36,5 +37,5 @@ app.post("/chat", async (req, res) => {
 
 const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  // console.log(`Server is running on http://localhost:${PORT}`);
 });
